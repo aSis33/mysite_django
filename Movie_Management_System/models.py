1e436b2fbd9f2d0dfdab34c7d9af5e7b54dd1535
Ashish Giri <giria1360@gmail.com>
2020-10-19 23:24:56 +0545

    Adding ability to Post from admin interface

--- Movie_Management_System/models.py
+++ Movie_Management_System/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Postmovie(models.Model):
+    movie_title = models.CharField(max_length=100)
+    movie_description = models.TextField()
+    movie_file = models.FileField(upload_to="media/movies")
+    release_date = models.DateField(blank=True, null=True)
+    Director_Name = models.CharField(max_length=100)
+    Actors_Name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.movie_title
