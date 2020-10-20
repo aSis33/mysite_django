c431601892c7bbc0b7cd61ed8bb0a6c084b85b73
Ashish Giri <giria1360@gmail.com>
2020-10-20 13:18:49 +0545

    Add details view for each Movie

--- Movie_Management_System/urls.py
+++ Movie_Management_System/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     #127.0.0.1:8000
-    path('', views.Movie_Post_List, name="movie_post_list")
+    path('', views.Movie_Post_List, name="movie_post_list"),
+    #127.0.0.1:8000/post/2(Primary key of movies)
+    path('post/<int:pk>/', views.Movie_detail, name="Movie_detail")
 ]
--- Movie_Management_System/views.py
+++ Movie_Management_System/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Postmovie
 # Create your views here.
@@ -6,3 +6,8 @@ def Movie_Post_List(request):
     posts = Postmovie.objects.filter(Published_date__lte = timezone.now()).order_by('-Published_date') ##Displaying posted movie by admin accroding to the published date
     stuff_for_frontend = {'posts':posts}
     return render(request, 'Movie_Management_System/Movie_Post_List.html', stuff_for_frontend)
+
+def Movie_detail(request, pk):
+    post = get_object_or_404(Postmovie, pk=pk) ##EITHER gets specific object from primary key or return 404
+    stuff_for_frontend = {'post': post}
+    return render(request, 'Movie_Management_System/Movie_details.html',stuff_for_frontend) #render movie details
